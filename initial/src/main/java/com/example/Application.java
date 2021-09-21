6d6b426f6522f19d72b3bea955e6f9dba8779e73
Jessica Simpson <jsimpson@scottlogic.com>
2021-09-21 15:38:27 +0100

    Add authorisation

--- initial/src/main/java/com/example/Application.java
+++ initial/src/main/java/com/example/Application.java
@@ -2,13 +2,21 @@ package com.example;
 
 import java.util.Arrays;
 
+
 import com.example.service.UserService;
+import com.example.springboot.JWTAuthorizationFilter;
 import com.example.springboot.User;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @SpringBootApplication
 public class Application {
@@ -23,6 +31,20 @@ public class Application {
 		Arrays.sort(beanNames);
 		for (String beanName : beanNames) {
 			System.out.println(beanName);
+		}
+	}
+
+	@EnableWebSecurity
+	@Configuration
+	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable()
+					.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+					.authorizeRequests()
+					.antMatchers(HttpMethod.POST, "/login").permitAll()
+					.anyRequest().authenticated();
 		}
 	}
 
--- initial/src/main/java/com/example/springboot/HelloController.java
+++ initial/src/main/java/com/example/springboot/HelloController.java
@@ -1,11 +1,14 @@
 package com.example.springboot;
 
 import com.example.service.UserService;
-//import io.jsonwebtoken.Jwts;
-//import io.jsonwebtoken.SignatureAlgorithm;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -96,11 +99,36 @@ public class HelloController {
 	}
 
 	@PostMapping("/login")
-	public boolean userLogin (@Valid @RequestBody User user) {
-		Login login= new Login();
-		List<String> users= userService.getAllUsernames();
-		List<String> passwords= userService.getAllPasswords();
-		boolean success=login.userLogin(user.getUsername(), user.getPassword(), users,passwords);
-		return success;
+	public String userLogin (@Valid @RequestBody User user) {
+		String token = getJWTToken(user.getUsername());
+		user.setToken(token);
+		return user.getToken();
+
+//		Login login= new Login();
+//		List<String> users= userService.getAllUsernames();
+//		List<String> passwords= userService.getAllPasswords();
+//		boolean success=login.userLogin(user.getUsername(), user.getPassword(), users,passwords);
+//		return success;
+	}
+
+	private String getJWTToken(String username) {
+		String secretKey = "mySecretKey";
+		List<GrantedAuthority> grantedAuthorities = AuthorityUtils
+				.commaSeparatedStringToAuthorityList("ROLE_USER");
+
+		String token = Jwts
+				.builder()
+				.setId("softtekJWT")
+				.setSubject(username)
+				.claim("authorities",
+						grantedAuthorities.stream()
+								.map(GrantedAuthority::getAuthority)
+								.collect(Collectors.toList()))
+				.setIssuedAt(new Date(System.currentTimeMillis()))
+				.setExpiration(new Date(System.currentTimeMillis() + 600000))
+				.signWith(SignatureAlgorithm.HS512,
+						secretKey.getBytes()).compact();
+
+		return "Bearer " + token;
 	}
 }
